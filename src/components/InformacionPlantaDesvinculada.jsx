8b90fec9fd5b44ff1fc928bc803dc09fb96f03e2
Alfonso Contreras <contrerasalfonso.lino@gmail.com>
2023-11-18 16:37:57 -0500

    sistemas

--- src/components/InformacionPlantaDesvinculada.jsx
+++ src/components/InformacionPlantaDesvinculada.jsx
@@ -11,19 +11,26 @@ export const InformacionPlantaDesvinculada = () => {
         setIdPlanta(selectedOption.value);
     }
 
-    const opciones = plantas.map((planta) => ({
-        value: planta._id,
-        label: (
-          <div className='w-full flex gap-5 items-center'>
-            <img className='w-4' src={`${import.meta.env.VITE_RUTA_BACKEND}${planta.pathIcono}`} alt="imagen" />
-            {planta.nombre}
-          </div>
-        ),
-      }));
+    const opciones = plantas.map((planta) => {
+        if(planta._id !== '6557f6371c63c6b93f684be7'){
+            return {
+
+                value: planta._id,
+                label: (
+                    <div className='w-full flex gap-5 items-center'>
+                        <img className='w-4' src={`${import.meta.env.VITE_RUTA_BACKEND}${planta.pathIcono}`} alt="imagen" />
+                        {planta.nombre}
+                    </div>
+                ),
+            
+            }  
+        }
+        return null;
+    }).filter(Boolean);;
 
   return (
     <>
-        <div className='w-fit bg-white p-5 mt-5 rounded-lg flex flex-col gap-5'>
+        <div className='w-96 bg-white p-5 mt-5 rounded-lg flex flex-col gap-5'>
             <h1 className='font-semibold max-lg:text-sm text-center'>Información de la planta</h1>
             <div className='flex gap-10 items-center'>
                 <div className='flex flex-col gap-2'>
@@ -32,8 +39,11 @@ export const InformacionPlantaDesvinculada = () => {
                     <p className='text-sm font-semibold'>Humedad: <span className='font-normal'>No disponible</span></p>
                     <p className='text-sm font-semibold'>Temperatura: <span className='font-normal'>No disponible</span></p>
                     <p className='text-sm font-semibold'>Suelo: <span className='font-normal'>No disponible</span></p>
-                    <div className='flex items-center gap-2'>
-                        <p className='text-sm font-semibold'>Planta:</p>
+
+                </div>
+                <img className='w-16' src={`https://img2.freepng.es/20180326/jyq/kisspng-computer-icons-clip-art-cross-5ab8913181c8f7.1853602815220452335316.jpg`} alt="planta img" />
+            </div>
+            <div className=''>
                         <Select
                             className='rounded-lg py-2 w-full'
                             value={opciones.find((opcion) => opcion.value === idPlanta)}
@@ -41,9 +51,6 @@ export const InformacionPlantaDesvinculada = () => {
                             options={opciones}
                             isSearchable={false}
                         />
-                    </div>
-                </div>
-                <img className='w-16' src={`https://img2.freepng.es/20180326/jyq/kisspng-computer-icons-clip-art-cross-5ab8913181c8f7.1853602815220452335316.jpg`} alt="planta img" />
             </div>
             <button className='text-sm border-emerald-500 border rounded-lg py-2 bg-emerald-500 text-white duration-300 hover:bg-emerald-600'>Vincular</button>
         </div>
