054e54d6a2897cabcfb98c17504c7853b61d69dc
Alfonso <contrerasalfonso.lino@gmail.com>
2023-12-06 19:04:16 -0500

    test

--- src/pages/Changelog.jsx
+++ src/pages/Changelog.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import { SimpleBarChart } from '../components/SimpleBarChart'
 
 export const Changelog = () => {
+
+  const [ruta,setRuta] = useState('sinmouse.mp4');
+  
+  const handleVideo = () => {
+    if(ruta == 'sinmouse.mp4'){
+        setRuta('conmouse.mp4')
+    }else{
+      setRuta('sinmouse.mp4')
+    }
+  }
+
   return (
     <div className='p-10'>
-        <SimpleBarChart/>
+        <button onClick={handleVideo} className='bg-slate-500 text-white rounded-lg py-3 px-3 hover:bg-slate-700'>Activar video mouse</button>
+        <video src={`/videos/${ruta}`} autoPlay  loop></video>
     </div>
   )
 }
